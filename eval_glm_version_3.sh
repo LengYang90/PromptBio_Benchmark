5e51fdd25a046b0852bdce85655924bf1ec4da41
yang.leng <yang.leng@yanglengdeMacBook-Air.local>
2026-08-14 17:56:34 +0800

    Add shell script

---
+++ eval_glm_version_3.sh
@@ -0,0 +1,6 @@
+python evaluate_task_with_deep_agent.py a-3-13
+python evaluate_task_with_deep_agent.py a-14-2
+python evaluate_task_with_deep_agent.py a-15-2
+python evaluate_task_with_deep_agent.py a-15-9
+python evaluate_task_with_deep_agent.py b-8-5
+python evaluate_task_with_deep_agent.py b-8-10
