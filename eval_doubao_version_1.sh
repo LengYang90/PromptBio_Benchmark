389b4e1639fb44a91dd703eac177acffc778991c
yang.leng <yang.leng@yanglengdeMacBook-Air.local>
2026-08-17 18:34:59 +0800

    Add faild doubao high difficulty quesion re-evalution results

---
+++ eval_doubao_version_1.sh
@@ -0,0 +1,12 @@
+python evaluate_task_with_deep_agent.py a-11-2 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-11-8 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-13-5 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-14-2 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-15-7 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-15-9 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-3-6 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-4-6 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-4-8 --result-dir results_doubao
+python evaluate_task_with_deep_agent.py a-7-7 --result-dir results_doubao
+
+
---
+++ shell/summary_failed_high_difficulty_doubao_deepagent.sh
@@ -0,0 +1,3 @@
+python ~/project/github/PromptBio_Benchmark/summarize_evaluations.py a-11-2 a-11-8 a-13-5 a-14-2 a-15-7 a-15-9 a-3-6 a-4-6 a-4-8 a-7-7 \
+  --result-dir results_doubao \
+  --output ~/project/github/PromptBio_Benchmark/reports/failed_high_difficulty_doubao_deepagent_evaluation_summary.csv
